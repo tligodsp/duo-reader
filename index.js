9f1185f7a0aa7ee5ebb1421f52d6ac6618026f34
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-10-31 22:44:48 +0700

    Added manga data as json, display manga cover

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 const electron = require("electron");
 const fs = require("fs");
 const path = require("path");
@@ -46,10 +47,39 @@ const createWindow = () => {
 
 app.on("ready", createWindow);
 
+function readMangasData(mangaPaths) {
+  mangaPaths.forEach(mangaDir => {
+    fs.readFile(path.join(LIBRARY_PATH, mangaDir, "data.json"), (err, res) => {
+      if (err) {
+        throw console.log(err);
+      }
+      const mangaData = JSON.parse(res);
+      mangas = [ ...mangas, mangaData ];
+      mainWindow.webContents.send("library:mangaList", mangas);
+    });
+  });
+  // for (let mangaDir of mangaPaths) {
+  //   // await fs.readFile(path.join(LIBRARY_PATH, mangaDir, "data.json"), (err, res) => {
+  //   //   if (err) {
+  //   //     throw err;
+  //   //   }
+  //   //   const mangaData = JSON.parse(res);
+  //   //   mangas = [ ...mangas, mangaData ];
+  //   //   console.log('in fs');
+  //   // })
+  //   const jsonContent = fs.readFileSync(path.join(LIBRARY_PATH, mangaDir, "data.json"), 'utf8');
+  //   const mangaData = JSON.parse(jsonContent);
+  //   mangas = [ ...mangas, mangaData ];
+  //   console.log(jsonContent);
+  //   console.log('in loop');
+  // }
+}
+
 ipcMain.on("library:getMangas", () => {
+  mangas = [];
   fs.readdir(LIBRARY_PATH, (err, items) => {
-    mangas = items.filter(mangaDir => fs.lstatSync(path.join(LIBRARY_PATH, mangaDir)).isDirectory());
+    let mangaPaths = items.filter(dir => fs.lstatSync(path.join(LIBRARY_PATH, dir)).isDirectory());
+    readMangasData(mangaPaths);
     mainWindow.webContents.send("library:mangaList", mangas);
   });
-  // mainWindow.webContents.send("library:mangaList", mangas);
 });
--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -7,9 +7,16 @@ const Library = (props) => {
     props.getMangas();
   }, []);
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       {props.library.mangas.map(manga => (
-        <div>{manga}</div>
+        <div style={{ margin: "10px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <img 
+            src={"file:///library/" + manga.id + "/" + manga.cover} 
+            alt={manga.id}
+            style={{ width: 190, height: 300, borderRadius: "5%" }}
+          />
+          <div style={{ color: "white" }}>{manga.title}</div>
+        </div>
       ))}
     </div>
 
