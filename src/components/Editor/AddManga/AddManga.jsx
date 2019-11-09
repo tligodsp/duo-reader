8ebfad3e2d718f9977cfebf2d5ed0b3e74df0ba6
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-11-09 22:59:21 +0700

    Implement Add Chapter Modal

--- src/components/Editor/AddManga/AddManga.jsx
+++ src/components/Editor/AddManga/AddManga.jsx
@@ -1,62 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Col, Button, Alert, InputGroup } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { useFormik } from 'formik';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Col, Button, Alert, InputGroup, Modal } from "react-bootstrap";
+import { connect } from "react-redux";
+import { useFormik } from "formik";
 
-import { getMangas } from '../../../actions/libraryActions';
-import { InputGroupAppend } from 'react-bootstrap/InputGroup';
+import { getMangas } from "../../../actions/libraryActions";
 const { ipcRenderer } = window.require("electron");
 const { dialog } = window.require("electron").remote;
 
-const AddManga = (props) => {
-
+const AddManga = props => {
   const validate = values => {
     const errors = {};
 
     if (!values.id) {
-      errors.id = 'Required';
-    }
-    else if (!/^[a-zA-Z0-9]*$/.test(values.id)) {
+      errors.id = "Required";
+    } else if (!/^[a-zA-Z0-9]*$/.test(values.id)) {
       errors.id = "Only letters and numbers plz";
-    }
-    else {
+    } else {
       const mangas = props.library.mangas;
       if (!(mangas.findIndex(manga => manga.id === values.id) === -1)) {
-        errors.id = 'Id already exists';
+        errors.id = "Id already exists";
       }
     }
 
     if (!values.title) {
-      errors.title = 'Required';
+      errors.title = "Required";
     }
 
     const d = new Date();
 
     if (values.publishYear && !/^\d+$/.test(values.publishYear)) {
-      errors.publishYear = 'Numbers only plz';
-    }
-    else if (values.publishYear && parseInt(values.publishYear, 10) < 1900) {
-      errors.publishYear = 'Wtf is this ancient shit?';
-    }
-    else if (values.publishYear && parseInt(values.publishYear, 10) > d.getFullYear()) {
-      errors.publishYear = 'Wow hello there time traveler';
+      errors.publishYear = "Numbers only plz";
+    } else if (values.publishYear && parseInt(values.publishYear, 10) < 1900) {
+      errors.publishYear = "Wtf is this ancient shit?";
+    } else if (
+      values.publishYear &&
+      parseInt(values.publishYear, 10) > d.getFullYear()
+    ) {
+      errors.publishYear = "Wow hello there time traveler";
     }
 
     return errors;
   };
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const formik = useFormik({
     initialValues: {
-      id: '',
-      title: '',
-      author: '',
-      artist: '',
-      cover: '',
-      description: '',
-      genres: '',
-      publishYear: '',
-      publishStatus: ''
+      id: "",
+      title: "",
+      author: "",
+      artist: "",
+      cover: "",
+      description: "",
+      genres: "",
+      publishYear: "",
+      publishStatus: ""
     },
     validate,
     onSubmit: values => {
@@ -92,20 +94,34 @@ const AddManga = (props) => {
 
   const test = () => {
     console.log();
-  }
+  };
 
   const onCoverButtonClick = () => {
-    const fileSelected = dialog.showOpenDialog({ properties: ['openFile'] });
-    const coverPath = fileSelected ? fileSelected[0].replace(/\\/g,"/") : '';
+    const fileSelected = dialog.showOpenDialog({ properties: ["openFile"] });
+    const coverPath = fileSelected ? fileSelected[0].replace(/\\/g, "/") : "";
     // console.log(coverPath);
     formik.setValues({
       ...formik.values,
       cover: coverPath
-    })
-  }
+    });
+  };
 
   return (
     <div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Link to="/editor">Back</Link>
       {/* <div style={{ color: "white" }}>{formData.title} {formData.author} {formData.artist}</div> */}
       <div style={{ display: "flex", justifyContent: "center" }}>
@@ -114,68 +130,76 @@ const AddManga = (props) => {
             <Alert.Heading>Éc</Alert.Heading>
             <p>
               Change this and that and try again. Duis mollis, est non commodo
-              luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-              Cras mattis consectetur purus sit amet fermentum.
+              luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+              elit. Cras mattis consectetur purus sit amet fermentum.
             </p>
           </Alert>
-          <Button onClick={test}>Click</Button>
+          <Button onClick={handleShow}>Click</Button>
           <Form onSubmit={formik.handleSubmit}>
-              <Form.Group>
-                <Form.Label>Id</Form.Label>
-                <Form.Control
-                  id="id"
-                  name="id"
-                  placeholder="Manga Id (This will be used for manga directory)" 
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.id}
-                  isInvalid={formik.touched.id && formik.errors.id}
-                  isValid={formik.touched.id && !formik.errors.id}
-                />
-                <Form.Control.Feedback type="invalid">{formik.errors.id}</Form.Control.Feedback>
-                <Form.Control.Feedback type="valid">Looking cool Joker</Form.Control.Feedback>
+            <Form.Group>
+              <Form.Label>Id</Form.Label>
+              <Form.Control
+                id="id"
+                name="id"
+                placeholder="Manga Id (This will be used for manga directory)"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.id}
+                isInvalid={formik.touched.id && formik.errors.id}
+                isValid={formik.touched.id && !formik.errors.id}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.id}
+              </Form.Control.Feedback>
+              <Form.Control.Feedback type="valid">
+                Looking cool Joker
+              </Form.Control.Feedback>
               {/* {formik.touched.id && formik.errors.id ? 
                 <Form.Control.Feedback type="invalid">{formik.errors.id}</Form.Control.Feedback> : null
               } */}
-              </Form.Group>
+            </Form.Group>
 
-              <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  id="title" 
-                  name="title" 
-                  placeholder="Manga Title" 
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.title}
-                  isInvalid={formik.touched.title && formik.errors.title}
-                  isValid={formik.touched.title && !formik.errors.title}
-                />
-                <Form.Control.Feedback type="invalid">{formik.errors.title}</Form.Control.Feedback>
-                <Form.Control.Feedback type="valid">Looking cool Joker</Form.Control.Feedback>
-              </Form.Group>
+            <Form.Group>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                id="title"
+                name="title"
+                placeholder="Manga Title"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.title}
+                isInvalid={formik.touched.title && formik.errors.title}
+                isValid={formik.touched.title && !formik.errors.title}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.title}
+              </Form.Control.Feedback>
+              <Form.Control.Feedback type="valid">
+                Looking cool Joker
+              </Form.Control.Feedback>
+            </Form.Group>
 
             <Form.Row>
               <Form.Group as={Col}>
                 <Form.Label>Author</Form.Label>
                 <Form.Control
                   id="author"
-                  name="author" 
-                  placeholder="Author Name" 
+                  name="author"
+                  placeholder="Author Name"
                   onChange={formik.handleChange}
                   value={formik.values.author}
                 />
                 <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
+                  Please choose a username.
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group as={Col}>
                 <Form.Label>Artist</Form.Label>
                 <Form.Control
                   id="artist"
-                  name="artist" 
-                  placeholder="Artist Name" 
+                  name="artist"
+                  placeholder="Artist Name"
                   onChange={formik.handleChange}
                   value={formik.values.artist}
                 />
@@ -188,12 +212,14 @@ const AddManga = (props) => {
                 <Form.Control
                   id="cover"
                   name="cover"
-                  placeholder="Cover Image Path" 
+                  placeholder="Cover Image Path"
                   onChange={formik.handleChange}
                   value={formik.values.cover}
                 />
                 <InputGroup.Append>
-                  <Button variant="secondary" onClick={onCoverButtonClick}>...</Button>
+                  <Button variant="secondary" onClick={onCoverButtonClick}>
+                    ...
+                  </Button>
                 </InputGroup.Append>
               </InputGroup>
             </Form.Group>
@@ -203,20 +229,20 @@ const AddManga = (props) => {
               <Form.Control
                 id="genres"
                 name="genres"
-                placeholder="List Genres, seperated by comma (temporary, will make it easier later)" 
-                onChange={formik.handleChange} 
+                placeholder="List Genres, seperated by comma (temporary, will make it easier later)"
+                onChange={formik.handleChange}
                 value={formik.values.genres}
               />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Manga Description</Form.Label>
-              <Form.Control 
-                as= "textarea"
+              <Form.Control
+                as="textarea"
                 rows="4"
                 id="description"
                 name="description"
-                placeholder="Manga Description" 
+                placeholder="Manga Description"
                 onChange={formik.handleChange}
                 value={formik.values.description}
               />
@@ -228,15 +254,23 @@ const AddManga = (props) => {
                 <Form.Control
                   id="publishYear"
                   name="publishYear"
-                  placeholder="Publish Year" 
+                  placeholder="Publish Year"
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.publishYear}
-                  isInvalid={formik.touched.publishYear && formik.errors.publishYear}
-                  isValid={formik.touched.publishYear && !formik.errors.publishYear}
+                  isInvalid={
+                    formik.touched.publishYear && formik.errors.publishYear
+                  }
+                  isValid={
+                    formik.touched.publishYear && !formik.errors.publishYear
+                  }
                 />
-                <Form.Control.Feedback type="invalid">{formik.errors.publishYear}</Form.Control.Feedback>
-                <Form.Control.Feedback type="valid">Looking cool Joker</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  {formik.errors.publishYear}
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="valid">
+                  Looking cool Joker
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group as={Col}>
@@ -256,11 +290,7 @@ const AddManga = (props) => {
               </Form.Group>
             </Form.Row>
 
-            <Button 
-              variant="primary" 
-              style={{ float: "right" }}
-              type="submit"
-            >
+            <Button variant="primary" style={{ float: "right" }} type="submit">
               Submit
             </Button>
           </Form>
@@ -270,8 +300,11 @@ const AddManga = (props) => {
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   library: state.library
 });
 
-export default connect(mapStateToProps, { getMangas })(AddManga);
+export default connect(
+  mapStateToProps,
+  { getMangas }
+)(AddManga);
--- src/components/MangaInfo/MangaInfo.jsx
+++ src/components/MangaInfo/MangaInfo.jsx
@@ -1,19 +1,37 @@
-import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Card } from 'react-bootstrap';
-import classes from './MangaInfo.module.scss';
-import { getCoverFullPath } from '../../utils/helpers';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { Card } from "react-bootstrap";
+import classes from "./MangaInfo.module.scss";
+import { getCoverFullPath } from "../../utils/helpers";
+import {
+  Button,
+  Modal,
+  Form,
+  Col,
+  InputGroup,
+  DropdownButton,
+  Dropdown
+} from "react-bootstrap";
+import { useFormik } from "formik";
+import { getCurrentManga, clearCurrentManga } from "../../actions/mangaActions";
 
-import { getCurrentManga, clearCurrentManga } from '../../actions/mangaActions';
-
-const MangaInfo = (props) => {
+const MangaInfo = props => {
   let { id } = useParams();
+  const [showAddChapterModal, setShowAddMangaModal] = useState(true);
+
+  const handleShowAddChapterModal = () => {
+    setShowAddMangaModal(true);
+  };
+
+  const handleCloseAddChapterModal = () => {
+    setShowAddMangaModal(false);
+  };
 
   useEffect(() => {
     props.getCurrentManga(id);
     return () => {
-      props.clearCurrentManga()
+      props.clearCurrentManga();
     };
   }, []);
 
@@ -24,100 +42,231 @@ const MangaInfo = (props) => {
       <Link to="/">Back</Link>
       {/* <div>{id}</div>
       <div>{props.manga.manga.description}</div> */}
-      <Card className={classes['manga-info-card']} bg="dark" text="white" border="dark">
+      <Card
+        className={classes["manga-info-card"]}
+        bg="dark"
+        text="white"
+        border="dark"
+      >
         <Card.Header>{manga.title}</Card.Header>
         <Card.Body>
-          <div className={classes['manga-info-container']}>
-            <div className={classes['manga-cover-container']}>
-              <img 
-                src={getCoverFullPath(manga.id, manga.cover)} 
+          <div className={classes["manga-info-container"]}>
+            <div className={classes["manga-cover-container"]}>
+              <img
+                src={getCoverFullPath(manga.id, manga.cover)}
                 alt={manga.id}
-                className={classes['manga-cover-img']} 
-              />  
+                className={classes["manga-cover-img"]}
+              />
             </div>
-            <div className={classes['manga-detail-container']}>
-
+            <div className={classes["manga-detail-container"]}>
               {/* alt names */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Alt name(s)</div>
-                <div className={classes['item-content']}>{manga.title}</div>
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Alt name(s)</div>
+                <div className={classes["item-content"]}>{manga.title}</div>
               </div>
-              <div className={classes['line']}></div>
+              <div className={classes["line"]}></div>
 
               {/* author */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Author</div>
-                <div className={classes['item-content']}>{manga.author}</div>
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Author</div>
+                <div className={classes["item-content"]}>{manga.author}</div>
               </div>
-              <div className={classes['line']}></div>
+              <div className={classes["line"]}></div>
 
               {/* artist */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Artist</div>
-                <div className={classes['item-content']}>{manga.artist}</div>
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Artist</div>
+                <div className={classes["item-content"]}>{manga.artist}</div>
               </div>
-              <div className={classes['line']}></div>
+              <div className={classes["line"]}></div>
 
               {/* genre */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Genre(s)</div>
-                <div className={classes['item-content']}>
-                  {
-                    manga.genres ? 
-                      manga.genres.map(genre => (
-                        <div className={classes['genre']} >{genre}</div>  
-                      )) 
-                    : null
-                  }
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Genre(s)</div>
+                <div className={classes["item-content"]}>
+                  {manga.genres
+                    ? manga.genres.map(genre => (
+                        <div className={classes["genre"]}>{genre}</div>
+                      ))
+                    : null}
                 </div>
               </div>
-              <div className={classes['line']}></div>
+              <div className={classes["line"]}></div>
 
               {/* description */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Description</div>
-                <div className={classes['item-content']}>{manga.description}</div>
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Description</div>
+                <div className={classes["item-content"]}>
+                  {manga.description}
+                </div>
               </div>
-              <div className={classes['line']}></div>
+              <div className={classes["line"]}></div>
 
               {/* released */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Released</div>
-                <div className={classes['item-content']}>{manga.publishYear}</div>
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Released</div>
+                <div className={classes["item-content"]}>
+                  {manga.publishYear}
+                </div>
               </div>
-              <div className={classes['line']}></div>
+              <div className={classes["line"]}></div>
 
               {/* publish status */}
-              <div className={classes['manga-detail-item']}>
-                <div className={classes['item-name']}>Pub. Status</div>
-                <div className={classes['item-content']}>{manga.publishStatus}</div>
+              <div className={classes["manga-detail-item"]}>
+                <div className={classes["item-name"]}>Pub. Status</div>
+                <div className={classes["item-content"]}>
+                  {manga.publishStatus}
+                </div>
               </div>
-              
             </div>
           </div>
-          <hr style={{ height: "1px", color: "black", backgroundColor: "#222" }}></hr>
+          <hr
+            style={{ height: "1px", color: "black", backgroundColor: "#222" }}
+          ></hr>
+          <Button onClick={handleShowAddChapterModal}>Add chapter</Button>
           <div>
-            { 
-              manga.chapters?
-                manga.chapters.map(chapter => (
-                <Link 
-                  className={classes['chapters-container']}
-                  to={ "/reader/" + manga.id + "/" + chapter.id }
-                >
-                  Vol. {chapter.volumeNumber} Ch.{chapter.chapterNumber} - {chapter.title}
-                </Link>
+            {manga.chapters
+              ? manga.chapters.map(chapter => (
+                  <Link
+                    className={classes["chapters-container"]}
+                    to={"/reader/" + manga.id + "/" + chapter.id}
+                  >
+                    Vol. {chapter.volumeNumber} Ch.{chapter.chapterNumber} -{" "}
+                    {chapter.title}
+                  </Link>
                 ))
-              : null 
-            }
+              : null}
           </div>
         </Card.Body>
       </Card>
+      <AddChapterModal
+        show={showAddChapterModal}
+        handleClose={handleCloseAddChapterModal}
+      ></AddChapterModal>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
+const AddChapterModal = props => {
+  let chapterByLangArr = [{ language: "", imagesPath: "" }];
+
+  const handleAddLanguageImagesPath = () => {
+    formik.setValues({
+      ...formik.values,
+      imagesPaths: [ ...formik.values.imagesPaths, { language: "", imagesPath: "" } ]
+    })
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      chapterId: "",
+      chapterTitle: "",
+      chapterNumber: "",
+      volumeNumber: "",
+      imagesPaths: [{ language: "", imagesPath: "" }]
+    }
+  });
+  return (
+    <Modal show={props.show} onHide={props.handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Chapter</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ padding: "12px 50px" }}>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Chapter number</Form.Label>
+              <Form.Control
+                id="chapterNumber"
+                name="chapterNumber"
+                placeholder="Chapter number"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.chapterNumber}
+              />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Volume number</Form.Label>
+              <Form.Control
+                id="volumeNumber"
+                name="volumeNumber"
+                placeholder="Volume number"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.volumeNumber}
+              />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group>
+            <Form.Label>Chapter Title</Form.Label>
+            <Form.Control
+              id="chapterTitle"
+              name="chapterTitle"
+              placeholder="Chapter Title"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.chapterTitle}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Chapter ID</Form.Label>
+            <Form.Control
+              id="chapterId"
+              name="chapterId"
+              placeholder="Chapter ID"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.chapterId}
+            />
+          </Form.Group>
+          
+          <Form.Label>Images path(s)</Form.Label>
+          {formik.values.imagesPaths.map(imagesPath => (
+            <Form.Row>
+              <Form.Group as={Col} md="3" controlId="formGridState">
+                <Form.Control as="select">
+                  <option>Lang</option>
+                  <option>...</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group as={Col}>
+                <InputGroup md="9">
+                  <Form.Control />
+                  <InputGroup.Append>
+                    <Button variant="outline-secondary">...</Button>
+                    <Button variant="outline-danger" >&times;</Button>
+                  </InputGroup.Append>
+                </InputGroup>
+              </Form.Group>
+            </Form.Row>
+          ))}
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              variant="outline-primary"
+              style={{ flex: "1" }}
+              onClick={handleAddLanguageImagesPath}
+            >+</Button>
+          </div>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.handleClose}>
+          Close
+        </Button>
+        <Button variant="primary">Add</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+const mapStateToProps = state => ({
   manga: state.manga
 });
 
-export default connect(mapStateToProps, { getCurrentManga, clearCurrentManga })(MangaInfo);
+export default connect(
+  mapStateToProps,
+  { getCurrentManga, clearCurrentManga }
+)(MangaInfo);
